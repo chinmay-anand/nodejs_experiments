82b274a9b36640fa09ca757c88a032a1e8f4e735
chinmay-anand <34304780+chinmay-anand@users.noreply.github.com>
2020-12-29 13:14:13 +0530

    Delete test_6_delay_using_timeout.js

--- test_6_delay_using_timeout.js
+++
@@ -1,8 +0,0 @@
-const waitTime = 3000;
-console.log(`Setting a ${waitTime/1000} delay.`);
-
-const timerFinished = () => {
-    console.log("Done");
-};
-
-setTimeout(timerFinished, waitTime); // Now the process will run asynchronously for the duration of waitTime milli seconds
